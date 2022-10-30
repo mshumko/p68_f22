78515b853ddc48ee920456bf309c478110ee1458
Mykhaylo <msshumko@gmail.com>
2022-10-30 15:44:34 -0400

    Got the integrator to integrate. Now I need to convert to field aligned coordinates.

--- bounce_motion/bounce.py
+++ bounce_motion/bounce.py
@@ -31,7 +31,7 @@ def dipole(X, x0=(0,0,0)):
     Bx = 3*M*dx*dz/r**5
     By = 3*M*dy*dz/r**5
     Bz = M*(3*dz**2-r**2)/r**5
-    return [Bx, By, Bz]
+    return np.array([Bx, By, Bz])
 
 def dqdt(X, t, q, m):
     """
@@ -69,38 +69,39 @@ def dqdt(X, t, q, m):
 
 
 if __name__ == '__main__':
-    q0 = (0, 4*R, 0, 0, 1, 1)  # pos, vel
+    q0 = (0, 4*R, 0, 1, 0, 1)  # pos, vel
 
-    # L = 5
-    # B = [0, 0, 3.12E-5*(1/L)**3]  # T
+    # # L = 5
+    # # B = [0, 0, 3.12E-5*(1/L)**3]  # T
 
     args = (q_e, m_e)  # q, m , B
-    t = np.linspace(0, 1, num=5000)
+    t = np.linspace(0, 0.1, num=5000)
     solution = odeint(dqdt, q0, t, args=args)
 
-    # Remove coordinates if the particle escaped.
-    r = np.linalg.norm(solution, axis=1)
-    trapped_idx = np.where(r < 10*R)[0]
-    solution = solution[trapped_idx, :]
-    print(np.min(r)/R, np.max(r)/R)
+    # # Remove coordinates if the particle escaped.
+    # r = np.linalg.norm(solution, axis=1)
+    # trapped_idx = np.where(r < 10*R)[0]
+    # solution = solution[trapped_idx, :]
+    # print(np.min(r)/R, np.max(r)/R)
 
     # # ax = plt.subplot(111, projection='3d')
     # # ax.plot3D(solution[:, 0], solution[:, 1], solution[:, 2])
     # plt.plot(solution[:, 0], solution[:, 2])
+    fig, ax = plt.subplots(3, 1, sharex=True)
+    for ax_i, solution_i in zip(ax, solution[:, :3].T):
+        ax_i.plot(t, solution_i, 'k')
+    plt.show()
 
-    # plt.show()
 
-
-    # y = R*np.arange(-10.0, 10.0, .1) #create a grid of points from y = -10 to 10
-    # z = R*np.arange(-10.0, 10.0, .1) #create a grid of points from z = -10 to 10
+    # y = R*np.linspace(-10.0, 10.0, 100) #create a grid of points from y = -10 to 10
+    # z = R*np.linspace(-10.0, 10.0, 100) #create a grid of points from z = -10 to 10
     # Y, Z = np.meshgrid(y,z) #turn this into a mesh
     # ilen, jlen = np.shape(Y) #define the length of the dimensions, for use in iteration
     # Bf = np.zeros((ilen,jlen,3)) #set the points to 0
 
     # for i in range(0, ilen): #iterate through the grid, setting each point equal to the magnetic field value there
     #     for j in range(0, jlen):
-    #         Bf[i,j] = dipole([0.0, Y[i,j], Z[i,j]]) 
+    #         Bf[i,j] = dipole([0.0, Y[i,j], Z[i,j]], (0, 0, -10*R)) + dipole([0.0, Y[i,j], Z[i,j]], (0, 0, 110*R)) 
             
     # plt.streamplot(Y,Z, Bf[:,:,1], Bf[:,:,2], color='k')
-    plt.plot(solution[:, 1], solution[:, 2], c='g')
     plt.show()
